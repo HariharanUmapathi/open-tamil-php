1c0a232b849483a1a8b4e8e17352bdd40b4868b1
HariharanUmapathi <smarthariharan28@gmail.com>
2024-11-08 00:36:43 +0530

    setup.php is updated to install php-cs-fixer

--- setup.php
+++ setup.php
@@ -1,3 +1,8 @@
 <?PHP 
 
-echo "Setting up development environment for open-tamil".PHP_EOL;
+echo "Setting up development environment for open-tamil-php".PHP_EOL;
+
+echo "Setting up PHP CS Fixer".PHP_EOL;
+
+system("mkdir -p tools/php-cs-fixer");
+system("composer require --working-dir=tools/php-cs-fixer friendsofphp/php-cs-fixer");
